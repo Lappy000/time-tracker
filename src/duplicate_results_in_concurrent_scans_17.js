785feabad488049037caea9b6b8bbe4346027691
Lappy000 <not.furry10@gmail.com>
2026-03-27 22:59:00 +0300

    fix: duplicate results in concurrent scans

---
+++ src/duplicate_results_in_concurrent_scans_17.js
@@ -0,0 +1,28 @@
+/**
+ * fix: duplicate results in concurrent scans
+ */
+
+class Handler17 {
+  constructor(config = {}) {
+    this.config = { enabled: true, timeout: 30000, retries: 3, ...config };
+    this._state = {};
+  }
+
+  async run(data) {
+    try {
+      return await this._process(data);
+    } catch (err) {
+      if (this.config.retries > 0) {
+        this.config.retries--;
+        return this.run(data);
+      }
+      throw err;
+    }
+  }
+
+  async _process(data) { return data; }
+
+  get state() { return { ...this._state }; }
+}
+
+module.exports = Handler17;
