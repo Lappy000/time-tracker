ff09f2c8ab807410046842d1d652403c32be2d9e
Lappy000 <not.furry10@gmail.com>
2026-05-31 12:23:00 +0300

    feat: add --json output format flag to cli

---
+++ src/cli/index.js
@@ -0,0 +1,40 @@
+/**
+ * cli module for time-tracker
+ * @module cli
+ */
+
+class CliHandler {
+  /**
+   * @param {Object} config - Configuration options
+   */
+  constructor(config = {}) {
+    this.config = config;
+    this.initialized = false;
+  }
+
+  /**
+   * Execute --json output format flag
+   * @param {...*} args - Arguments
+   * @returns {Promise<*>} Result
+   */
+  async execute(...args) {
+    if (!this.initialized) {
+      await this._init();
+    }
+    return this._run(...args);
+  }
+
+  async _init() {
+    this.initialized = true;
+  }
+
+  _run(...args) {
+    throw new Error('Not implemented');
+  }
+
+  get isReady() {
+    return this.initialized;
+  }
+}
+
+module.exports = { CliHandler };
